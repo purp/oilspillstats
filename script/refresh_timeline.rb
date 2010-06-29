27ff5ba375df267c43db98e49a71d379e0f5a55b
Jim Meyer <jim@geekdaily.org>
2010-06-29 11:09:29 -0700

    Add update time to page

--- script/refresh_timeline.rb
+++ script/refresh_timeline.rb
@@ -10,6 +10,7 @@ var last_event_time = new Date(Date.parse(#{tl.end_time.to_simile}));
 var tl_start = first_event_time; 
 var tl_end = new Date(Date.parse(#{(tl.end_time + 1.week).to_simile}));
 var elapsed_days = #{((Time.now - tl.start_time) / 1.day).truncate};
+var update_time = #{{Time.now.to_simile}};
 
 var timeline_data = {  // save as a global variable
     'dateTimeFormat': 'iso8601',
