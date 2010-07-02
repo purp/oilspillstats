3910a79452eb9cd8da76cdf34f3ef9add8bc7029
Jim Meyer <jim@geekdaily.org>
2010-07-01 22:51:36 -0700

    Tweak views to match schema and for pleasingness

--- lib/core_ext.rb
+++ lib/core_ext.rb
@@ -3,3 +3,9 @@ class Time
     self.to_formatted_s("%m/%d/%Y %H:%M:%S %Z").to_json
   end
 end
+
+class String
+  def truncate(len = 20, marker = '...')
+    self.size > len ? self[0..(len - marker.size - 1)] + marker : self
+  end
+end
